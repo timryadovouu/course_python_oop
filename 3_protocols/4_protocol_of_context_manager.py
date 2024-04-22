292f8679d99f7b3b2384bca2e3decea997bb8ebc
timar <timaryadovou@yandex.ru>
2024-04-22 19:21:59 +0300

    220424 - win

--- 3_protocols/4_protocol_of_context_manager.py
+++ 3_protocols/4_protocol_of_context_manager.py
@@ -25,6 +25,13 @@ def bar_0():
         print(manager)
         print(manager[100])
 
+    # Вход в контекстный менеджер...
+    # Python generation!
+    # Выход из контекстного менеджера...
+    # <class 'IndexError'>
+    # string index out of range
+    # <traceback object at 0x000001E5CBB57500>
+
 
 def bar_1():
     """Обработка исключений внутри блока with"""
@@ -91,7 +98,7 @@ def bar_5():
 
 
 def bar_6():
-    """обеспечивает примитивную блокировку для предотвращения одновременного изменения общего ресурса
+    """Обеспечивает примитивную блокировку для предотвращения одновременного изменения общего ресурса
      несколькими потоками в многопоточном приложении.
      Создает защищенную область (критическая секция), которая предотвращает одновременный доступ из разных
     потоков к общим ресурсам."""
@@ -136,7 +143,7 @@ def bar_8():
 
         def __enter__(self):
             print('Вход в контекстный менеджер...')
-            self.name = 'Кемаль'
+            self.name = 'Камаль'
             self.breed = 'Британский'
             return self
 
@@ -360,20 +367,19 @@ def task_4():
 def task_5():
     class Reloopable:
         def __init__(self, ffile):
-           self.file = ffile
+            self.file = ffile
 
         def __enter__(self):
             return self.file.readlines()
 
         def __exit__(self, exc_type, exc_val, exc_tb):
             self.file.close()
-    
 
     with open('file.txt', 'w') as file:
         file.write('Evil is evil\n')
         file.write('Lesser, greater, middling\n')
         file.write('Makes no difference\n')
-        
+
     with Reloopable(open('file.txt')) as reloopable:
         for line in reloopable:
             print(line.strip())
@@ -382,16 +388,247 @@ def task_5():
 
 
 def task_6():
-    pass
+    import sys
+
+    class UpperPrint:
+        def __enter__(self):
+            self.standard_output = sys.stdout.write
+            sys.stdout.write = lambda text: self.standard_output(text.upper())
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            sys.stdout.write = self.standard_output
+
+    print('Если жизнь одаривает вас лимонами — не делайте лимонад')
+    print('Заставьте жизнь забрать их обратно!')
+
+    with UpperPrint():
+        print('Мне не нужны твои проклятые лимоны!')
+        print('Что мне с ними делать?')
+
+    print('Требуйте встречи с менеджером, отвечающим за жизнь!')
 
 
 def task_7():
-    pass
+    class Suppress:
+        def __init__(self, *args):
+            self.exceptions = args
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            if exc_type in self.exceptions:
+                self.exception = exc_val
+                return True
+            self.exception = None
+            return False
+
+    # TEST_8:
+    try:
+        with Suppress(ValueError) as context:
+            number = list(123)
+    except TypeError:
+        pass
+
+    print(context.exception)
 
 
 def task_8():
-    pass
+    class WriteSpy:
+        def __init__(self, file1, file2, to_close=False):
+            self.f1, self.f2 = file1, file2
+            self.to_close = to_close
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            if self.to_close is True:
+                self.f1.close()
+                self.f2.close()
+
+        def write(self, text):
+            try:
+                self.f1.write(text)
+                self.f2.write(text)
+            except Exception:
+                raise ValueError("Файл закрыт или недоступен для записи")
+
+        def close(self):
+            self.f1.close()
+            self.f2.close()
+
+        def writable(self):
+            if self.f1.closed or self.f2.closed:
+                return False
+            return self.f1.writable() and self.f2.writable()
+
+        def closed(self):
+            return self.f1.closed and self.f2.closed
+
+    # TEST_5:
+    f1 = open('file1.txt', mode='r')
+    f2 = open('file2.txt', mode='w')
+
+    try:
+        with WriteSpy(f1, f2, to_close=True) as combined:
+            combined.write('No cost too great')
+    except ValueError as error:
+        print(error)
 
 
 def task_9():
+    from copy import deepcopy
+
+    class Atomic:
+        def __init__(self, data, deep=False):
+            """data — произвольный список, множество или словарь"""
+            self.data = data
+            self.deep = deep
+            self.recover = deepcopy(self.data) if deep else self.data.copy()
+
+        def __enter__(self):
+            return self.recover
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            if exc_type:
+                return True
+            if isinstance(self.recover, list):
+                self.data[:] = self.recover
+            else:
+                self.data.clear()
+                self.data.update(self.recover)
+            return False
+
+    numbers = [1, 2, 3, 4, 5]
+    with Atomic(numbers) as atomic:
+        atomic.append(6)
+        atomic[2] = 0
+        del atomic[1]
+    print(numbers)
+    #
+    numbers = [1, 2, 3, 4, 5]
+    with Atomic(numbers) as atomic:
+        atomic.append(6)
+        atomic[2] = 0
+        del atomic[100]  # обращение по несуществующему индексу
+    print(numbers)
+
+
+"""==================ПРОТОКОЛ-КОНТЕКСТНЫХ-МЕНЕДЖЕРОВ===============PART3==============SUMMARY========================"""
+
+
+# Одноразовые контекстные менеджеры
+# они могут эффективно использоваться в операторе with только один раз
+def foo_5():
+    """ValueError: I/O operation on closed file."""
+    file = open('output.txt', mode='w', encoding='utf-8')
+    with file:
+        file.write('Python generation!')
+
+    with file:
+        file.write('Python generation!')
+
+
+# Многоразовые контекстные менеджеры
+# менеджер, который можно повторно использовать в рамках невложенных операторов with
+def foo_6():
+    from time import perf_counter, sleep
+    class Timer:
+        def __enter__(self):
+            self.start = perf_counter()
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            self.elapsed = perf_counter() - self.start
+
+    timer = Timer()
+    with timer:
+        sleep(1.5)
+    print('Затраченное время:', timer.elapsed)
+
+    with timer:
+        sleep(0.7)
+    print('Затраченное время:', timer.elapsed)
+
+    with timer:
+        sleep(1)
+    print('Затраченное время:', timer.elapsed)
+
+
+# Реентерабельные контекстные менеджеры
+#  это менеджер, который можно повторно использовать в рамках вложенных операторов with
+
+def foo_7():
+    class Indenter:
+        def __init__(self):
+            self.level = -1
+
+        def __enter__(self):
+            self.level += 1
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            self.level -= 1
+
+        def print(self, text):
+            print('    ' * self.level + text)
+
+    with Indenter() as indent:
+        indent.print('python')
+        with indent:
+            indent.print('beegeek')
+            with indent:
+                indent.print('stepik')
+            indent.print('pygen')
+        indent.print('bye-bye')
+
+
+"""==================ПРОТОКОЛ-КОНТЕКСТНЫХ-МЕНЕДЖЕРОВ===============PART3==============TASKS=========================="""
+
+
+def task_10():
+    from time import perf_counter
+
+    class AdvancedTimer:
+        def __init__(self):
+            self.last_run = None
+            self.runs = []
+            self.min = None
+            self.max = None
+
+        def __enter__(self):
+            self.start = perf_counter()
+            return self
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            # self.elapsed = perf_counter() - self.start
+            self.last_run = perf_counter() - self.start
+            self.runs.append(self.last_run)
+            self.min = min(self.runs)
+            self.max = max(self.runs)
+
+    from time import sleep
+
+    timer = AdvancedTimer()
+
+    with timer:
+        sleep(1.5)
+
+    with timer:
+        sleep(0.7)
+
+    with timer:
+        sleep(1)
+
+    print([round(runtime, 1) for runtime in timer.runs])
+    print(round(timer.min, 1))
+    print(round(timer.max, 1))
+
+
+def task_11():
+    pass
+
+
+def task_12():
     pass
--- help.py
+++ help.py
@@ -1,1 +1,35 @@
-pass
+class HtmlTag:
+    def __init__(self, tag, inline=False):
+        self.tag = tag
+        self.inline = inline
+        # self.level = -1
+        self.new_line = "\n"
+
+    level = -1
+
+    def __enter__(self):
+        self.__class__.level += 1
+        print('  ' * self.__class__.level + f"<{self.tag}>", end=f"{self.new_line * (not self.inline)}")
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('  ' * self.__class__.level * (not self.inline) + f"</{self.tag}>")
+        self.__class__.level -= 1
+
+    def print(self, text):
+        self.__class__.level += 1
+        print(f"{'  ' * (not self.inline) * self.level}{text}", end=f"{self.new_line * (not self.inline)}")
+        self.__class__.level -= 1
+
+
+with HtmlTag('body') as _:
+    with HtmlTag('h1') as header:
+        header.print('Поколение Python')
+    with HtmlTag('p') as section:
+        section.print('Cерия курсов по языку программирования Python от команды BEEGEEK')
+
+with HtmlTag('body') as _:
+    with HtmlTag('h1', True) as header:
+        header.print('Поколение Python')
+    with HtmlTag('p', True) as section:
+        section.print('Cерия курсов по языку программирования Python от команды BEEGEEK')
